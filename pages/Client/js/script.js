b280aab07851c10fc565cc6c4475b498f78ef91c
Adham Elsegeeny <adham_elsegeeny@icloud.com>
2023-05-29 20:13:50 +0300

    client done

--- pages/Client/js/script.js
+++ pages/Client/js/script.js
@@ -1,16 +1,18 @@
 
 
 const payBillsButtons = Array.from(document.querySelectorAll(".green-button"));
-const swal = require('sweetalert2')
+var currentButton;
+
+function saveButton(button) {
+    currentButton = button;
+}
 
 
 function login() {
     var username = document.getElementById("email").value;
     var password = document.getElementById("password").value;
     if (username == "client" && password == "client" && true) {
-        alert("Login successfully");
         window.location.href = "../html/home.html";
-        //window.location = "success.html";
 
         return false;
     }
@@ -20,119 +22,129 @@ function login() {
 }
 
 
-function submit(){
+function submit() {
     alert("Submitted Succesfully");
-    
-}
-
-function goToHome(){
-    window.location.href="../html/home.html";
-}
-
-function payCreditDue(){
-
-    var all= document.getElementById('selectAll');
-    var card1= document.getElementById('card1');
-    var card2= document.getElementById('card2');
-    if(card1.checked || card2.checked || all.checked){
-        prompt("Enter Bank Account Number For Payment");
-        prompt("Enter Amount");
-        alert("Payment Successful");
-        window.location.href = "../html/cards.html";
-    }
-    else{
-        alert("Please Select a card");
-    }
-    
 
 }
 
-function reportLoss(){
+function goToHome() {
+    window.location.href = "../html/home.html";
+}
 
-    var all= document.getElementById('selectAll');
-    var card1= document.getElementById('card1');
-    var card2= document.getElementById('card2');
-    if(card1.checked || card2.checked || all.checked){
+function payCreditDue() {
+
+    alert("Payment Successful");
+
+
+}
+
+function reportLoss() {
+
+    var all = document.getElementById('selectAll');
+    var card1 = document.getElementById('card1');
+    var card2 = document.getElementById('card2');
+    if (card1.checked || card2.checked || all.checked) {
         alert("Reported Successfully");
         window.location.href = "../html/cards.html";
     }
-    else{
+    else {
         alert("Please Select a card");
     }
 
 }
 
-function report(){
+function report() {
     alert("Reported Successfully");
     window.location.href = "../html/bills.html";
 }
 
-function payBill(){
-    const card= document.getElementById("card");
-    const acc= document.getElementById("acc");
-    const amountC= document.getElementById("amountC");
-    const amountB= document.getElementById("amountB");
+function payBill() {
+    const card = document.getElementById("card");
+    const acc = document.getElementById("acc");
     // alert(card.value!="")
-    if(card.value!=""){
-        if(amountC.value==""){
-            alert("Please Enter Amount");
-        }
-        else{
-            // const button= document.getElementById("bill1");
-            // button.disabled=true;
-            alert("Payment Successful");
-            
-        }
+    if (card.value != "") {
+
+        // const button= document.getElementById("bill1");
+        // button.disabled=true;
+        alert("Payment Successful");
+        var parentElement = currentButton.parentNode;
+        var parentElement2 = parentElement.parentNode;
+        parentElement2.classList.add("visually-hidden");
+
+
     }
-    else if(acc.value!=""){
-        if(amountB.value==""){
-            alert("Please Enter Amount");
-        }
-        else{
-            // const button= document.getElementById("bill1");
-            // button.disabled=true;
-            alert("Payment Successful");
-        }
+    else if (acc.value != "") {
+
+
+        // const button= document.getElementById("bill1");
+        // button.disabled=true;
+        alert("Payment Successful");
+        var parentElement = currentButton.parentNode;
+        var parentElement2 = parentElement.parentNode;
+        parentElement2.classList.add("visually-hidden");
     }
 
-    else{
+    else {
         alert("Please Select a card or account");
     }
 }
 
-function transferType(){
-    const type= document.getElementById("Transferlist");
-    if(type.value=="1"){
+function transferType() {
+    const type = document.getElementById("Transferlist");
+    if (type.value == "1") {
         prompt("Enter Bank Name");
     }
-    else if(type.value=="2"){
+    else if (type.value == "2") {
         prompt("Enter Country Name");
         prompt("Enter Bank Name");
     }
 }
 
 
-function transfer(){
+function transfer() {
     alert("Transfer Completed Successfully");
 }
 
-function redeemPoints(){
-    var all= document.getElementById('selectAll');
-    var card1= document.getElementById('card1');
-    var card2= document.getElementById('card2');
-    if(card1.checked || card2.checked || all.checked){
+function redeemPoints() {
+    var all = document.getElementById('selectAll');
+    var card1 = document.getElementById('card1');
+    var card2 = document.getElementById('card2');
+    if (card1.checked || card2.checked || all.checked) {
         prompt("Enter Bank Account Number To recieve the cash On");
         alert("Redeemed Successfully");
         window.location.href = "../html/cards.html";
     }
-    else{
+    else {
         alert("Please Select a card");
     }
 
 }
 
-function reportTransaction(){
+function reportTransaction() {
     prompt("Enter Transaction Number");
     alert("Reported Successfully");
+}
+
+function openAccount() {
+    prompt("Savings Or Checking");
+    alert("Account Opened Successfully");
+    // window.location.href= "../html/account.html";
+    var element = document.getElementById("newAccount");
+    element.classList.remove("visually-hidden");
+}
+
+function closeAccount() {
+
+    var all = document.getElementById('selectAll');
+    var card1 = document.getElementById('card1');
+    var card2 = document.getElementById('card2');
+    if (card1.checked || card2.checked || all.checked) {
+        alert("Account Closed Successfully");
+        window.location.href = "../html/account.html";
+    }
+    else {
+        alert("Please Select an Account");
+    }
+
 }
 
